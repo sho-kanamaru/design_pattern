06f846f7974cfc8704db98ae4c10129815ea83e8
sho <sho02.soccer@gmail.com>
2019-07-03 10:03:06 +0900

    create animals and plants dir

---
+++ AbstractFactory/main.rb
@@ -0,0 +1,24 @@
+require '../AbstractFactory/factories/FrogAndAlgaeFactory'
+require '../AbstractFactory/factories/DuckAndWaterLilyFactory'
+
+factory = FrogAndAlgaeFactory.new(4,1)
+animals = factory.get_animals
+animals.each { |animal| animal.eat }
+#=> カエル 動物 0 は食事中です
+#=> カエル 動物 1 は食事中です
+#=> カエル 動物 2 は食事中です
+#=> カエル 動物 3 は食事中です
+plants = factory.get_plants
+plants.each { |plant| plant.grow }
+#=> 藻 植物 0 は成長中です
+
+factory = DuckAndWaterLilyFactory.new(3,2)
+animals = factory.get_animals
+animals.each { |animal| animal.eat }
+#=> アヒル 動物 0 は食事中です
+#=> アヒル 動物 1 は食事中です
+#=> アヒル 動物 2 は食事中です
+plants = factory.get_plants
+plants.each { |plant| plant.grow }
+#=> スイレン 植物 0 は成長中です
+#=> スイレン 植物 1 は成長中です
